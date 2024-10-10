0ad403a8eebd4250ac8982fc553abc7f8e1c6813
최찬환 <unsc2438@gmail.com>
2024-10-10 16:23:50 +0900

    style: Init shadcn, tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,20 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
+  darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
-  plugins: []
+  theme: {
+    colors: {
+      transparent: "transparent",
+      black: "hsl(0, 0%, 9%, 0.8)",
+      white: "hsl(0, 0%, 96%, 0.8)",
+      green: "hsla(140, 71%, 45%, 0.8)",
+      yellow: "hsla(45, 85%, 47%, 0.8)",
+      red: "hsla(0, 82%, 60%, 0.8)"
+    }
+  },
+  plugins: [tailwindcssAnimate]
 };
+
 export default config;
